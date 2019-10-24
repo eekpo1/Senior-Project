1521ef0e64ec08b1c9b7d2551ca0df498af03b25
kryptonpwn <edwin321@ymail.com>
2019-10-24 11:27:41 -0700

    updated student repository

--- src/main/kotlin/com/tablesdemo/repository/StudentRepository.kt
+++ src/main/kotlin/com/tablesdemo/repository/StudentRepository.kt
@@ -4,11 +4,17 @@ import com.tablesdemo.model.Student
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 @RepositoryRestResource
 interface StudentRepository : JpaRepository<Student, Long> {
     fun findByFirstName(firstName: String): Student
 
-    fun findByLastName(lastName: String): Student
+    fun findByFirstNameAndLastName(firstName: String, lastName: String): Optional<Student>
+
+    fun findByUsername(usernname: String): Optional<Student>
+
+    fun findAllByCoursesIn(id: Long): List<Student>
+
 }
