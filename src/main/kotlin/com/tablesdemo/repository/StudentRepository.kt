18360ee83cb6b3eaa2087265e19415e79c3c3e11
kryptonpwn <edwin321@ymail.com>
2019-10-24 10:31:14 -0700

    Merge branch 'master' of C:\Users\edwin\IdeaProjects\Senior-Project with conflicts.

--- src/main/kotlin/com/tablesdemo/repository/StudentRepository.kt
+++ src/main/kotlin/com/tablesdemo/repository/StudentRepository.kt
@@ -4,11 +4,11 @@ import com.tablesdemo.model.Student
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 import org.springframework.stereotype.Repository
-import java.util.*
 
 @Repository
 @RepositoryRestResource
 interface StudentRepository : JpaRepository<Student, Long> {
-    fun findByFirstNameAndLastName(first: String, last:String): Optional<Student>
-    fun findByUsername(username: String): Optional<Student>
+    fun findByFirstName(firstName: String): Student
+
+    fun findByLastName(lastName: String): Student
 }
